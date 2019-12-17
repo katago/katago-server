7f6d674c0267655efc4012f07d5eb70be2afc2ca
tychota <tychota@users.noreply.github.com>
2019-12-17 01:47:41 +0100

    Base api for user and groups

---
+++ katago_server/users/api.py
@@ -0,0 +1,24 @@
+
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
+
+from katago_server.users.models import User
+from katago_server.users.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
---
+++ katago_server/users/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group
+from rest_framework import serializers
+
+from katago_server.users.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["url", "username", "email", "groups"]
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ["url", "name"]
+
