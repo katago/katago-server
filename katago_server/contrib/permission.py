66615f4331c0bd7c9452d6104eeb8775ba706e5b
tychota <tychota@users.noreply.github.com>
2019-12-24 01:30:24 +0100

    Refactor "Readonly" permission out of the user app

---
+++ katago_server/contrib/permission.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
