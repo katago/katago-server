1b833d96e24f0fb0b6ad0b22154dc7c3806c83ac
tychota <tychota@users.noreply.github.com>
2019-12-24 16:08:21 +0100

    Improve the models based on feedbacks

--- katago_server/games/models.py
+++ katago_server/games/models.py
@@ -1,12 +1,32 @@
 from enum import Enum
 
-from django.db.models import Model, CharField, DecimalField, IntegerField
+from django.contrib.postgres.fields import ArrayField, JSONField
+from django.db.models import Model, CharField, DecimalField, IntegerField, FileField, BooleanField
 
 
-class GamesRulesType(Enum):
-    JAPANESE = 'Japanese'
-    CHINESE = 'Chinese'
-    TROMP_TAYLOR = 'Trump Taylor'
+class KoRulesType(Enum):
+    SIMPLE = 'Simple'
+    POSITIONAL = 'Positional'
+    SITUATIONAL = 'Situational'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
+class ScoringRulesType(Enum):
+    AREA = 'Area'
+    TERRITORY = 'Territory'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
+class TaxRulesType(Enum):
+    NONE = 'None'
+    SEKI = 'Seki'
+    ALL = 'All'
 
     @classmethod
     def choices(cls):
@@ -17,7 +37,7 @@ class GamesResultType(Enum):
     WHITE = 'W'
     BLACK = 'B'
     JIGO = '0'
-    # https://senseis.xmp.net /?NoResult
+    # https://senseis.xmp.net/?NoResult
     MOSHOUBOU = '∅'
 
     @classmethod
@@ -25,25 +45,41 @@ class GamesResultType(Enum):
         return [(key.value, key.name) for key in cls]
 
 
-class Games(Model):
+class Game(Model):
     """
     This abstract class holds the games property that are common, no matter if the game is a self play, a branched game,
     a match or an human game.
     """
-    rules = CharField(max_length=20, choices=GamesRulesType.choices(), null=False, default=GamesRulesType.CHINESE)
+    board_size = ArrayField(IntegerField(null=False, default=19), size=2)
     handicap = IntegerField(null=False, default=0)
     komi = DecimalField(max_digits=3, decimal_places=1, null=False, default=7.0)
+
+    ko_rule = CharField(max_length=15, choices=KoRulesType.choices(), null=False)
+    scoring_rule = CharField(max_length=15, choices=ScoringRulesType.choices(), null=False)
+    tax_rule = CharField(max_length=15, choices=TaxRulesType.choices(), null=False)
+    multi_stone_suicide_allowed = BooleanField()
+    extra_rules = JSONField()
+
     result = CharField(max_length=15, choices=GamesResultType.choices(), null=False)
+    has_resigned = BooleanField()
     score = DecimalField(max_digits=4, decimal_places=1)
+
+    sgf_file = FileField()
 
     class Meta:
         abstract = True
 
 
-class SelfPlay(Games):
-    pass
-
-
-class Match(Games):
+class Match(Game):
     class Meta:
         verbose_name_plural = 'Matches'
+
+
+class SelfPlay(Game):
+    unpacked_training_file = FileField()
+    packed_training_file = FileField()
+
+
+class ForkedGame(SelfPlay):
+    parent_sgf_file = FileField()
+    forked_extra_params = JSONField()
