67ec4e03895f6a2a7e79a77468ca5a25b01d830c
tychota <tychota@users.noreply.github.com>
2019-12-24 01:13:25 +0100

    Install admin doc from django to have a documentation of the code inline

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,6 +62,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
+    "django.contrib.admindocs"
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
