7c5c6ac628a2811bf7a6bd3769622c3ed920a664
tychota <tychota@users.noreply.github.com>
2020-01-02 22:35:10 +0100

    Improve api quality by embedding networks link

--- katago_server/trainings/serializers.py
+++ katago_server/trainings/serializers.py
@@ -7,3 +7,9 @@ class NetworkSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Network
         fields = '__all__'
+
+
+class LimitedNetworkSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Network
+        fields = ['url', 'uuid', 'model_file']
