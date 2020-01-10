c6cc24760a7a233c718adba905288acea0720ef7
tychota <tychota@users.noreply.github.com>
2020-01-10 20:54:36 +0100

    Simplify DistributedTask by a lot, as katago is in charge of choosing most of the params

--- katago_server/distributed_efforts/models.py
+++ katago_server/distributed_efforts/models.py
@@ -1,5 +1,4 @@
 import os
-from enum import Enum
 
 import uuid as uuid
 from django.contrib.postgres.fields import JSONField
@@ -46,19 +45,9 @@ class DistributedTask(Model):
     # The networks related to this game
     white_network = ForeignKey(Network, verbose_name=_("network white"), on_delete=PROTECT, related_name='%(class)s_predefined_jobs_as_white')
     black_network = ForeignKey(Network, verbose_name=_("network black"),  on_delete=PROTECT, related_name='%(class)s_predefined_jobs_as_black')
-    # Describe the board/game itself
-    board_size_x = IntegerField(_("board absciss"), default=19)
-    board_size_y = IntegerField(_("board ordinate"), default=19)
-    handicap = IntegerField(_("nb of handicap stone"), default=0)
-    komi = DecimalField(_("komi (white)"), max_digits=3, decimal_places=1, default=7.0)
-    # -- Everything under is nullable --
-    rules_params = JSONField(_("game rules"), help_text=_("Depending on rule set, the ko (https://senseis.xmp.net/?Ko), the scoring (https://senseis.xmp.net/?Scoring), the group tax (https://senseis.xmp.net/?GroupTax) and the suicide (https://senseis.xmp.net/?Suicide) may have subtle difference. See more info here https://lightvector.github.io/KataGo/rules.html"), default=dict, null=True, blank=True)  # See https://lightvector.github.io/KataGo/rules.html
     # A PredefinedJob can be forked from an existing game or a initial situation
     initial_position_sgf_file = FileField(_("initial position, as sgf file"), upload_to=upload_initial_to, validators=(validate_sgf,), null=True, blank=True)
     initial_position_extra_params = JSONField(_("initial position extra parameters"), default=dict, null=True, blank=True)
-    # Some extra information about the game like the type
-    # eg: komi compensated games, uncompensated games, asymmetric playout games, seki-training games
-    game_extra_params = JSONField(_("extra game parameters regarding game, like number of playout"), default=dict, null=True, blank=True)
 
     def __str__(self):
         return f"{self.uuid}"
