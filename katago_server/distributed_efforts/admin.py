b5b2d5f9fb393337361438bfe9b47360f008700b
tychota <tychota@users.noreply.github.com>
2020-01-10 20:54:36 +0100

    Simplify DistributedTask by a lot, as katago is in charge of choosing most of the params

--- katago_server/distributed_efforts/admin.py
+++ katago_server/distributed_efforts/admin.py
@@ -9,27 +9,18 @@ from katago_server.distributed_efforts.models import RankingEstimationGameDistri
 @admin.register(RankingEstimationGameDistributedTask)
 @admin.register(TrainingGameDistributedTask)
 class GameDistributedTaskAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'created_at', 'assigned_to', 'assigned_at', 'expire_at', 'komi', 'handicap', 'white_network', 'black_network')
-    list_filter = ("created_at", "assigned_at", 'expire_at', "handicap", "komi")
+    list_display = ('uuid', 'created_at', 'assigned_to', 'assigned_at', 'expire_at', 'white_network', 'black_network')
+    list_filter = ("created_at", "assigned_at", 'expire_at')
     readonly_fields = ("id", "created_at", "uuid")
     ordering = ("pk",)
     fieldsets = (
         (None, {
             'fields': (('id', 'uuid', 'created_at'),)
         }),
-        (_("Game"), {
-            'fields': (('board_size_x', 'board_size_y'), ('handicap', 'komi'), 'rules_params')
-        }),
         (_("Network"), {
             'fields': (('white_network', 'black_network'),)
         }),
     )
-
-
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:  # Only set added_by during the first save.
-            obj.submitted_by = request.user
-        super().save_model(request, obj, form, change)
 
 
 admin.site.register(DynamicDistributedTaskConfiguration, SingletonModelAdmin)
