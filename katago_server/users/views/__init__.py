3c64dbc8a3b120a408edc6b8338b2387fe2e720a
tychota <tychota@users.noreply.github.com>
2020-03-08 15:14:39 +0100

    Fix tests

--- katago_server/users/views/__init__.py
+++ katago_server/users/views/__init__.py
@@ -1,3 +1,3 @@
-from .user_update import user_update_view
-from .user_detail import user_detail_view
-from .user_redirect import user_redirect_view
+from .user_update import user_update_view, UserUpdateView
+from .user_detail import user_detail_view, UserDetailView
+from .user_redirect import user_redirect_view, UserRedirectView
