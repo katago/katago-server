c8a47b59073631faf6052a94fd5bff0ea8cb6f6d
David Wu <lightvector@gmail.com>
2020-03-23 23:59:48 +0000

    Add fields to runs that are needed for client

--- katago_server/runs/models/run.py
+++ katago_server/runs/models/run.py
@@ -7,7 +7,6 @@ from django.utils.translation import gettext_lazy as _
 class Run(Model):
     created_at = DateTimeField(_("creation date"), auto_now_add=True)
     name = CharField(_("name"), max_length=64)
-
-
-
-
+    data_board_len = IntegerField(_("data board len"), default=19)
+    inputs_version = IntegerField(_("inputs version for model features"), default=7)
+    max_search_threads_allowed = IntegerField(_("max search threads server promises to never exceed"), default=8)
