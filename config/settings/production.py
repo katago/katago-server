ba0b2d6919854469ea67ab83a5e8a19aebfd8d6e
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2020-04-20 01:32:31 +0200

    Log postgres and django to datadog

--- config/settings/production.py
+++ config/settings/production.py
@@ -132,13 +132,14 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
     "formatters": {"verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"}},
-    "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "verbose",}},
+    "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "verbose"}},
     "root": {"level": "INFO", "handlers": ["console"]},
     "loggers": {
-        "django.db.backends": {"level": "ERROR", "handlers": ["console"], "propagate": False,},
+        "django.db.backends": {"level": "INFO", "handlers": ["console"], "propagate": False},
+        'ddtrace': {'level': 'INFO', 'handlers': ['console']},
         # Errors logged by the SDK itself
         "sentry_sdk": {"level": "ERROR", "handlers": ["console"], "propagate": False},
-        "django.security.DisallowedHost": {"level": "ERROR", "handlers": ["console"], "propagate": False,},
+        "django.security.DisallowedHost": {"level": "INFO", "handlers": ["console"], "propagate": False},
     },
 }
 
