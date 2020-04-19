8b9f85bec9f3589df33f73f3c827d03506449ead
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2020-04-19 17:39:53 +0200

    Let user id be uuid

--- katago_server/users/models/user.py
+++ katago_server/users/models/user.py
@@ -1,13 +1,13 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, UUIDField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
+    id= UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = CharField(_("Name of User"), blank=True, max_length=255)
 
     def get_absolute_url(self):
