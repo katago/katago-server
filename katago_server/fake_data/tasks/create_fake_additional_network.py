2f26414946dc536c5531011927d9f4836243f6e3
David Wu <lightvector@gmail.com>
2020-05-04 03:43:06 +0000

    Disable fake data

--- katago_server/fake_data/tasks/create_fake_additional_network.py
+++ katago_server/fake_data/tasks/create_fake_additional_network.py
@@ -6,8 +6,9 @@ from katago_server.trainings.models import Network
 
 @celery_app.task()
 def create_fake_additional_network():
-    current_run = Run.objects.select_current()
-    last_network = Network.objects.filter(run=current_run).last()
+    pass
+    # current_run = Run.objects.select_current()
+    # last_network = Network.objects.filter(run=current_run).last()
 
-    new_net = Network(parent_network_id=last_network.id, model_file="https://d3dndmfyhecmj0.cloudfront.net/g170/neuralnets/g170-b6c96-s175395328-d26788732.bin.gz", run=current_run)
-    new_net.save()
+    # new_net = Network(parent_network_id=last_network.id, model_file="https://d3dndmfyhecmj0.cloudfront.net/g170/neuralnets/g170-b6c96-s175395328-d26788732.bin.gz", run=current_run)
+    # new_net.save()
