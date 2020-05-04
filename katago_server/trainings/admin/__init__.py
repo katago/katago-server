d97cf86284660a457d6d1f70d523205781fa6404
David Wu <lightvector@gmail.com>
2020-05-04 03:30:09 +0000

    Finish ranking -> rating

--- katago_server/trainings/admin/__init__.py
+++ katago_server/trainings/admin/__init__.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 
 from katago_server.trainings.admin.network_admin import NetworkAdmin
-from katago_server.trainings.models import Network, NetworkBayesianRankingConfiguration
+from katago_server.trainings.models import Network, NetworkBayesianRatingConfiguration
 
 admin.site.register(Network, NetworkAdmin)
-admin.site.register(NetworkBayesianRankingConfiguration, SingletonModelAdmin)
+admin.site.register(NetworkBayesianRatingConfiguration, SingletonModelAdmin)
