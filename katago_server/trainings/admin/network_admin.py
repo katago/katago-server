60c4a006a4dc4905d8714804c1a44e13d262ba65
David Wu <lightvector@gmail.com>
2020-05-04 02:05:59 +0000

    Clean up and refactor network fields

--- katago_server/trainings/admin/network_admin.py
+++ katago_server/trainings/admin/network_admin.py
@@ -4,13 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 class NetworkAdmin(admin.ModelAdmin):
     list_display = ("id", "run", "name", "created_at", "size", "log_gamma_upper_confidence", "log_gamma_lower_confidence", "ranking")
-    list_filter = ("run", "created_at", "network_size", "nb_parameters")
-    readonly_fields = ("id", "created_at", "name", "log_gamma_upper_confidence", "log_gamma_lower_confidence")
+    list_filter = ("run", "created_at", "network_size")
+    readonly_fields = ("id", "created_at", "name", "log_gamma", "log_gamma_uncertainty", "log_gamma_upper_confidence", "log_gamma_lower_confidence")
     ordering = ("log_gamma_upper_confidence",)
     fieldsets = (
         (None, {"fields": (("id", "name", "created_at", "run"), "parent_network")}),
-        (_("Download"), {"fields": ("model_file","model_file_bytes","model_file_sha256")}),
+        (_("Model File"), {"fields": ("model_file","model_file_bytes","model_file_sha256")}),
         (_("Strength"), {"fields": (("log_gamma", "log_gamma_uncertainty"), "log_gamma_upper_confidence")}),
-        (_("Network architecture"), {"fields": (("network_size", "nb_parameters"), "model_architecture_details")}),
+        (_("Network architecture"), {"fields": ("network_size","is_random")}),
     )
 
