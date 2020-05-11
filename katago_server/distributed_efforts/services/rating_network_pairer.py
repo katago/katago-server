c127fd9918226d77445ea2cc45b696f84e7be25b
David Wu <lightvector@gmail.com>
2020-05-11 00:13:37 +0000

    Handle having too few networks, and also handle todos on pairing logic

--- katago_server/distributed_efforts/services/rating_network_pairer.py
+++ katago_server/distributed_efforts/services/rating_network_pairer.py
@@ -15,7 +15,7 @@ class RatingNetworkPairerService:
         """
         Generate a pairing of networks to play a rating game for a client task.
 
-        :return: Tuple of (white_network,black_network)
+        :return: Tuple of (white_network,black_network), or None if no pairing could be generated
         """
         if random.random() < self.current_run.rating_game_high_elo_probability:
             return self.generate_high_elo_game()
@@ -26,10 +26,12 @@ class RatingNetworkPairerService:
         """
         In order to decrease the uncertainty of top network, we can chose to invest more on network having a chance to be the best ones.
 
-        :return: Tuple of (white_network,black_network)
+        :return: Tuple of (white_network,black_network), or None if no pairing could be generated
         """
         reference_network = Network.objects.select_one_of_the_best_with_uncertainty(self.current_run)
         opponent_network = self._choose_opponent(reference_network)
+        if reference_network is None or opponent_network is None:
+            return None
         if random.random() < 0.5:
             return reference_network, opponent_network
         else:
@@ -39,10 +41,12 @@ class RatingNetworkPairerService:
         """
         In order to decrease the uncertainty of all network, we can look the one with biggest uncertainty.
 
-        :return: Tuple of (white_network,black_network)
+        :return: Tuple of (white_network,black_network), or None if no pairing could be generated
         """
         reference_network = Network.objects.select_one_of_the_more_uncertain(self.current_run)
         opponent_network = self._choose_opponent(reference_network)
+        if reference_network is None or opponent_network is None:
+            return None
         if random.random() < 0.5:
             return reference_network, opponent_network
         else:
@@ -55,11 +59,14 @@ class RatingNetworkPairerService:
         Once we have all that, we look for a network close enough, by applying shannon entropy: https://imgur.com/v9Q4By7
 
         :param reference_network:
-        :return: a network, chosen to be used as an opponent
+        :return: a network, chosen to be used as an opponent, or None if no distinct opponent could be found
         """
-        ref_net_log_gamma = reference_network.log_gamma  # TODO should I use log_gamma or log_gamma + uncertainty ?
+        if reference_network is None:
+            return None
 
-        log_gamma_search_range = 1200 / (400 * log10(e))  # TODO: should I make this configurable
+        ref_net_log_gamma = reference_network.log_gamma
+
+        log_gamma_search_range = 1200 / (400 * log10(e)) # Hardcoded window of 1200 Elo, we could dehardcode if needed in the future
         log_gamma_lower_bound = ref_net_log_gamma - log_gamma_search_range
         log_gamma_upper_bound = ref_net_log_gamma + log_gamma_search_range
 
@@ -68,10 +75,23 @@ class RatingNetworkPairerService:
             .filter(run=self.current_run, log_gamma__lte=log_gamma_upper_bound, log_gamma__gte=log_gamma_lower_bound,)
             .all()
         )
-        if len(nearby_networks) < 5:
-            nearby_networks_a = Network.objects.filter(run=self.current_run, pk__lt=reference_network.pk).all()[:20]
-            nearby_networks_b = Network.objects.filter(run=self.current_run, pk__gt=reference_network.pk).all()[:20]
-            return np.random.choice(nearby_networks_a + nearby_networks_b)
+        if len(nearby_networks) < 4:
+            nearby_weaker_networks = (
+                Network.objects.exclude(pk=reference_network.pk)
+                .filter(run=self.current_run, log_gamma__lte=ref_net_log_gamma)
+                .order_by("-log_gamma")
+                .all()[:2]
+            )
+            nearby_stronger_networks = (
+                Network.objects.exclude(pk=reference_network.pk)
+                .filter(run=self.current_run, log_gamma__gte=ref_net_log_gamma)
+                .order_by("log_gamma")
+                .all()[:2]
+            )
+            nearby_networks = nearby_weaker_networks + nearby_stronger_networks
+            if len(nearby_networks) <= 0:
+                return None
+            return np.random.choice(nearby_networks)
 
         win_probability = np.asarray([1 / (1 + exp(opp_net.log_gamma - ref_net_log_gamma)) for opp_net in nearby_networks])
         shannon_entropy = -win_probability * np.log2(win_probability) - (1 - win_probability) * np.log2(1 - win_probability)
