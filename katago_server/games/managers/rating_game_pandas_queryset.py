3f416859aa4a817c90f50e97a3523b0af7a8822e
David Wu <lightvector@gmail.com>
2020-06-02 22:23:47 +0000

    Fix bug where result should be winner in game queryset

--- katago_server/games/managers/rating_game_pandas_queryset.py
+++ katago_server/games/managers/rating_game_pandas_queryset.py
@@ -28,7 +28,7 @@ class RatingGamePandasQuerySet(QuerySet):
 
     def get_total_wins_count_as_white(self, run: Run):
         RatingGame = apps.get_model("games.RatingGame")
-        is_white_win = Q(result=RatingGame.GamesResult.WHITE)
+        is_white_win = Q(winner=RatingGame.GamesResult.WHITE)
         total_wins_as_white_count_aggregate = Count("id", filter=is_white_win)
         return (
             self.filter(run=run)
@@ -38,7 +38,7 @@ class RatingGamePandasQuerySet(QuerySet):
 
     def get_total_wins_count_as_black(self, run: Run):
         RatingGame = apps.get_model("games.RatingGame")
-        is_black_win = Q(result=RatingGame.GamesResult.BLACK)
+        is_black_win = Q(winner=RatingGame.GamesResult.BLACK)
         total_wins_as_black_count_aggregate = Count("id", filter=is_black_win)
         return (
             self.filter(run=run)
@@ -48,7 +48,7 @@ class RatingGamePandasQuerySet(QuerySet):
 
     def get_total_draw_or_no_result(self, run: Run):
         RatingGame = apps.get_model("games.RatingGame")
-        is_draw_or_no_result = Q(result=RatingGame.GamesResult.DRAW) | Q(result=RatingGame.GamesResult.NO_RESULT)
+        is_draw_or_no_result = Q(winner=RatingGame.GamesResult.DRAW) | Q(winner=RatingGame.GamesResult.NO_RESULT)
         total_draw_or_no_result_count_aggregate = Count("id", filter=is_draw_or_no_result)
         return (
             self.filter(run=run)
