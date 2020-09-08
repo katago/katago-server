aae4d6a522e00bb0a8b702488bc1039580111fc0
David Wu <lightvector@gmail.com>
2020-09-08 22:38:33 +0000

    Set network rating to parent on first upload

--- katago_server/trainings/serializers/network.py
+++ katago_server/trainings/serializers/network.py
@@ -5,7 +5,7 @@ from katago_server.trainings.models import Network
 
 class NetworkSerializer(HyperlinkedModelSerializer):
     """
-    Serializer for general display of networks for their info pages
+    Serializer for general display of networks for their info pages, and upload.
     """
 
     class Meta:
@@ -26,6 +26,8 @@ class NetworkSerializer(HyperlinkedModelSerializer):
             "parent_network",
             "log_gamma",
             "log_gamma_uncertainty",
+            "log_gamma_lower_confidence",
+            "log_gamma_upper_confidence",
         ]
         extra_kwargs = {
             "url": {"lookup_field": "name"},
@@ -33,6 +35,15 @@ class NetworkSerializer(HyperlinkedModelSerializer):
             "parent_network": {"lookup_field": "name"}
         }
 
+    def create(self, validated_data):
+        data = validated_data.copy()
+        if data["parent_network"]:
+            data["log_gamma"] = data["parent_network"].log_gamma
+            data["log_gamma_uncertainty"] = 2.0
+            data["log_gamma_lower_confidence"] = data["log_gamma"] - 2 * data["log_gamma_uncertainty"]
+            data["log_gamma_upper_confidence"] = data["log_gamma"] + 2 * data["log_gamma_uncertainty"]
+
+        return super().create(data)
 
 class NetworkSerializerForTasks(HyperlinkedModelSerializer):
     """
