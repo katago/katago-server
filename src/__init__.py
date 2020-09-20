c91a2ef49439f02f6f5dc8ab6f9234d070ffe200
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2020-09-20 21:13:54 +0200

    chore: moves entry point files to /src/

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,6 @@
+__version__ = "0.1.0"
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace("-", ".", 1).split(".")])
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery_app import app as celery_app
--- src/celery_app.py
+++ src/celery_app.py
@@ -2,9 +2,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings.local")
 
-app = Celery("katago_server")
+app = Celery("src")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
