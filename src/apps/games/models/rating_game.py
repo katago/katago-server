a6bec2204a1c714889d23c12934e679dfc85ab5a
David Wu <lightvector@gmail.com>
2020-10-04 17:46:34 +0000

    Delete training data and sgfs when games are deleted from the database

--- src/apps/games/models/rating_game.py
+++ src/apps/games/models/rating_game.py
@@ -1,5 +1,9 @@
+import os
+
 from django.db.models import Manager
 from django.utils.translation import gettext_lazy as _
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_delete
 
 from src.apps.games.managers.rating_game_pandas_manager import RatingGamePandasManager
 from src.apps.games.models.abstract_game import AbstractGame
@@ -17,3 +21,24 @@ class RatingGame(AbstractGame):
         verbose_name = _("Rating game")
         ordering = ["-created_at"]
 
+
+@receiver(post_delete, sender=RatingGame)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.sgf_file:
+        if os.path.isfile(instance.sgf_file.path):
+            os.remove(instance.sgf_file.path)
+
+@receiver(pre_save, sender=RatingGame)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = RatingGame.objects.get(pk=instance.pk).sgf_file
+    except RatingGame.DoesNotExist:
+        return False
+
+    new_file = instance.sgf_file
+    if old_file != new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
