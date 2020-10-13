58c454f3db9171283a9951346d086dc705d2e0dc
David Wu <lightvector@gmail.com>
2020-10-13 22:02:06 +0000

    Disable user view tests for now since route is no longer set up

--- src/apps/users/tests/test_models.py
+++ src/apps/users/tests/test_models.py
@@ -4,5 +4,5 @@ from django.conf import settings
 pytestmark = pytest.mark.django_db
 
 
-def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
+# def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
+#     assert user.get_absolute_url() == f"/users/{user.username}/"
