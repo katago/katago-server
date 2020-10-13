32d2d7c6616e7cb243a315d2669c936fde8bce1b
David Wu <lightvector@gmail.com>
2020-10-13 22:16:58 +0000

    Actually removing unused views and tests and urls

--- src/apps/users/views/__init__.py
+++ src/apps/users/views/__init__.py
@@ -1,3 +1,1 @@
-from .user_update import user_update_view, UserUpdateView
-from .user_detail import user_detail_view, UserDetailView
-from .user_redirect import user_redirect_view, UserRedirectView
+
