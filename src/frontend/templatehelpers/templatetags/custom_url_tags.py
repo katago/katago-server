1b0905c5fb33fba0c1e734e9f6b6a8f47c3bf144
David Wu <lightvector@gmail.com>
2020-10-22 02:56:04 +0000

    Move download template link stuff to a better place, remove extra notes column

--- src/frontend/templatehelpers/templatetags/custom_url_tags.py
+++ src/frontend/templatehelpers/templatetags/custom_url_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import gettext_lazy as _
 
 register = template.Library()
 
@@ -25,4 +26,13 @@ def rating_games_list_by_user(run_name, user_name):
 @register.simple_tag
 def sgfplayer(kind,id_number):
     return f"/frontend/sgfplayer/{kind}-games/{id_number}/"
+
+
+download_translated = _("Download")
+
+@register.filter
+def file_download_link_html(uploaded_file_obj):
+    if uploaded_file_obj:
+        return f'<a href="{uploaded_file_obj.url}">[{download_translated}]</a>'
+    return ""
 
