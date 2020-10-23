c477bdd411bc83439520be139214990bc0b3480f
David Wu <lightvector@gmail.com>
2020-10-23 23:55:46 +0000

    Add a view of contributions in the last week

--- src/frontend/templatehelpers/templatetags/custom_translation.py
+++ src/frontend/templatehelpers/templatetags/custom_translation.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 n_training_games_format = _("%d training games")
 n_rating_games_format = _("%d rating games")
+n_training_rows_format = _("%d data rows generated")
 
 # Peformance hack to avoid calling block translate tons of times in template in a loop
 
@@ -15,4 +16,8 @@ def as_n_training_games_str(value):
 @register.filter
 def as_n_rating_games_str(value):
   return n_rating_games_format % value
+
+@register.filter
+def as_n_training_rows_str(value):
+  return n_training_rows_format % value
 
--- src/frontend/views/contributions_view.py
+++ src/frontend/views/contributions_view.py
@@ -1,13 +1,13 @@
 from django.views.generic import ListView
 from django.db.models import Sum
 
-from src.apps.games.models import GameCountByUser
+from src.apps.games.models import GameCountByUser, RecentGameCountByUser
 
 from . import view_utils
 
 class ContributionsView(ListView):
   template_name = "pages/contributions.html"
-  context_object_name = "user_list"
+  context_object_name = "all_time_user_list"
 
   def get_queryset(self):
     return \
@@ -15,9 +15,27 @@ class ContributionsView(ListView):
       .objects \
       .all() \
       .values("username") \
-      .annotate(total_num_training_games=Sum("total_num_training_games"), total_num_rating_games=Sum("total_num_rating_games")) \
-      .order_by("-total_num_training_games")
+      .annotate(
+        total_num_training_rows=Sum("total_num_training_rows"),
+        total_num_training_games=Sum("total_num_training_games"),
+        total_num_rating_games=Sum("total_num_rating_games"),
+      ) \
+      .order_by("-total_num_training_rows")
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
+    context["recent_user_list"] = (
+      RecentGameCountByUser \
+      .objects \
+      .all() \
+      .values("username") \
+      .annotate(
+        total_num_training_rows=Sum("total_num_training_rows"),
+        total_num_training_games=Sum("total_num_training_games"),
+        total_num_rating_games=Sum("total_num_rating_games"),
+      ) \
+      .order_by("-total_num_training_rows")
+      .all()[:20]
+    )
+
     return context
