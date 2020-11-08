335f6f204e08d656b98047694a5ccbfef3eac173
David Wu <lightvector@gmail.com>
2020-11-08 02:48:44 +0000

    Add django allow cidr

--- src/settings/production.py
+++ src/settings/production.py
@@ -18,6 +18,8 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+# https://pypi.org/project/django-allow-cidr/
+ALLOWED_CIDR_NETS = env.list("DJANGO_ALLOWED_CIDR_NETS", default=[])
 
 # "site" domain for cookiecutters migration, which populates the "sites" db on the very first migration after
 # the site is built with these values, which is used in a production setting for full absolute URL building and
