a8f4a395e3cdaa37994aa3f48e0a00a06f36ca1c
David Wu <lightvector@gmail.com>
2020-11-08 20:00:10 +0000

    Update a bunch of requirements

--- src/settings/production.py
+++ src/settings/production.py
@@ -164,10 +164,12 @@ SENTRY_DSN = env("SENTRY_DSN")
 SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 
 sentry_logging = LoggingIntegration(
-    level=SENTRY_LOG_LEVEL, event_level=logging.ERROR,  # Capture info and above as breadcrumbs  # Send errors as events
+    level=SENTRY_LOG_LEVEL,     # Capture info and above as breadcrumbs
+    event_level=logging.ERROR,  # Send errors as events
 )
 sentry_sdk.init(
-    dsn=SENTRY_DSN, integrations=[sentry_logging, DjangoIntegration(), CeleryIntegration()],
+    dsn=SENTRY_DSN,
+    integrations=[sentry_logging, DjangoIntegration(), CeleryIntegration()],
 )
 
 # Your stuff...
