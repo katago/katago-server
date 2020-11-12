21b2b8afb85021769016114d6218f91b4806e4ea
David Wu <lightvector@gmail.com>
2020-11-12 00:23:46 +0000

    SSL policy

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -eux
 
-# This is how we installed at the start
+# This is how we installed at the start:
 # helm install katago-server katago-server --values katago-server/values_private.yaml
+
+# And also:
+# kubectl apply -f managedcert.yaml
