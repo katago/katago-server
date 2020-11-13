ec4d29de70e7df5b2a4966f4d26b3362c91ec354
David Wu <lightvector@gmail.com>
2020-11-13 03:52:01 +0000

    Minor improve build script

--- build_images_for_gcp.sh
+++ build_images_for_gcp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -eux
 
 IMAGEREPOPATH=$(cat imagerepopath.txt | tr -d '\n')
+IMAGETAG=$(git describe --abbrev=7 --tags --always --first-parent)
 
 docker build \
-       -t "$IMAGEREPOPATH"/katago-django:$(git describe --abbrev=7 --tags --always --first-parent) \
+       -t "$IMAGEREPOPATH"/katago-django:"$IMAGETAG" \
        -t "$IMAGEREPOPATH"/katago-training-server/katago-django:latest \
        . \
        -f compose/production/django/Dockerfile
 
 docker build \
-       -t "$IMAGEREPOPATH"/katago-nginx:$(git describe --abbrev=7 --tags --always --first-parent) \
+       -t "$IMAGEREPOPATH"/katago-nginx:"$IMAGETAG" \
        -t "$IMAGEREPOPATH"/katago-nginx:latest \
        . \
        -f compose/production/nginx/Dockerfile
 
+echo "$IMAGETAG"
