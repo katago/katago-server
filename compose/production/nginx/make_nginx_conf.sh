47afa52e235b839bd3e73505dd55f0120af04e6a
David Wu <lightvector@gmail.com>
2020-11-13 03:49:59 +0000

    Fix more nginx startup typos

--- compose/production/nginx/make_nginx_conf.sh
+++ compose/production/nginx/make_nginx_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-cat /etc/nginx/nginx.conf.template | envsubst -v '${NGINX_REAL_IPS} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
-cat /etc/nginx/conf.d/default.conf.template | envsubst -v '${NGINX_REAL_IPS} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/nginx.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
+cat /etc/nginx/conf.d/default.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
 
 exec "$@"
