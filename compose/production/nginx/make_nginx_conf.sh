644ed0e89310c7ca59763bc5dcf95a10ddec0922
David Wu <lightvector@gmail.com>
2020-11-13 05:32:50 +0000

    Allow nonratelimited nginx

--- compose/production/nginx/make_nginx_conf.sh
+++ compose/production/nginx/make_nginx_conf.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -eu
 
-cat /etc/nginx/nginx.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
-cat /etc/nginx/conf.d/default.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
+if [ "${NGINX_DO_RATE_LIMIT}" -eq "1" ]
+then
+    cat /etc/nginx/nginx.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
+    cat /etc/nginx/conf.d/default.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
+else
+    cat /etc/nginx/nginx.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
+    cat /etc/nginx/conf.d/defaultnoratelimit.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/defaultnoratelimit.conf
+fi
 
 exec "$@"
