4aaeee5acbc7d4bcffa88a274caceedb43099459
David Wu <lightvector@gmail.com>
2020-11-13 05:44:11 +0000

    Fix typo in script

--- compose/production/nginx/make_nginx_conf.sh
+++ compose/production/nginx/make_nginx_conf.sh
@@ -6,7 +6,7 @@ then
     cat /etc/nginx/conf.d/default.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
 else
     cat /etc/nginx/nginx.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
-    cat /etc/nginx/conf.d/defaultnoratelimit.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/defaultnoratelimit.conf
+    cat /etc/nginx/conf.d/defaultnoratelimit.conf.template | envsubst '${NGINX_TRUSTED_IP_PROXY_LIST} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
 fi
 
 exec "$@"
