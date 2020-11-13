d288e6cd0f5cdc95dd98fabf495f3357c49dde05
David Wu <lightvector@gmail.com>
2020-11-13 03:32:58 +0000

    Try fixing again

--- compose/production/nginx/make_nginx_conf.sh
+++ compose/production/nginx/make_nginx_conf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 cat /etc/nginx/nginx.conf.template | envsubst -v '${NGINX_REAL_IPS} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/nginx.conf
 cat /etc/nginx/conf.d/default.conf.template | envsubst -v '${NGINX_REAL_IPS} ${NGINX_REAL_IP_HEADER}' > /etc/nginx/conf.d/default.conf
