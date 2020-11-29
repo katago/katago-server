74dd7a347ff76bdd5608286870e57bcaadd0785f
David Wu <lightvector@gmail.com>
2020-11-29 18:33:02 +0000

    Fix up some formatting bugs, improve some games lists

--- src/frontend/templatehelpers/templatetags/custom_translation.py
+++ src/frontend/templatehelpers/templatetags/custom_translation.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 n_training_games_format = _("%d training games")
 n_rating_games_format = _("%d rating games")
+n_games_format = _("%d games")
 n_training_rows_format = _("%d rows generated")
 
 # Peformance hack to avoid calling block translate tons of times in template in a loop
@@ -16,6 +17,10 @@ def as_n_training_games_str(value):
 @register.filter
 def as_n_rating_games_str(value):
   return n_rating_games_format % value
+
+@register.filter
+def as_n_games_str(value):
+  return n_games_format % value
 
 @register.filter
 def as_n_training_rows_str(value):
