519b7d91891c658375ce63ba7f5cdbf7c6892062
Alexey <475367+Nihisil@users.noreply.github.com>
2021-01-26 11:44:40 +0800

    Format number of games output

--- src/frontend/templatehelpers/templatetags/custom_translation.py
+++ src/frontend/templatehelpers/templatetags/custom_translation.py
@@ -1,31 +1,32 @@
 from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
 from django.utils.translation import gettext_lazy as _
 
 register = template.Library()
 
-n_training_games_format = _("%d training games")
-n_rating_games_format = _("%d rating games")
-n_games_format = _("%d games")
-n_training_rows_format = _("%d rows generated")
+n_training_games_format = _("%s training games")
+n_rating_games_format = _("%s rating games")
+n_games_format = _("%s games")
+n_training_rows_format = _("%s rows generated")
 
 # Peformance hack to avoid calling block translate tons of times in template in a loop
 
 
 @register.filter
 def as_n_training_games_str(value):
-    return n_training_games_format % value
+    return n_training_games_format % intcomma(value)
 
 
 @register.filter
 def as_n_rating_games_str(value):
-    return n_rating_games_format % value
+    return n_rating_games_format % intcomma(value)
 
 
 @register.filter
 def as_n_games_str(value):
-    return n_games_format % value
+    return n_games_format % intcomma(value)
 
 
 @register.filter
 def as_n_training_rows_str(value):
-    return n_training_rows_format % value
+    return n_training_rows_format % intcomma(value)
