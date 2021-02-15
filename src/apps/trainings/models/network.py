5f58dd70c380cdc5cda97eb01b54df1db31a3086
Kuang-che Wu <kcwu@chromium.org>
2021-02-16 04:56:47 +0800

    make rating follow elo_precision

--- src/apps/trainings/models/network.py
+++ src/apps/trainings/models/network.py
@@ -215,15 +215,19 @@ class Network(Model):
     )
 
     def __str__(self):
-        return f"{self.name} ({self.elo}±{2 * self.elo_uncertainty:.1})"
+        return f"{self.name} ({self.elo:.{self.elo_precision}f}±{2 * self.elo_uncertainty:.{self.elo_precision}f})"
 
     @property
     def size(self):
         return f"{self.network_size}"
 
     @property
+    def elo_precision(self):
+        return 1
+
+    @property
     def elo(self):
-        return round(self.log_gamma * 400 * log10(e), ndigits=1)
+        return self.log_gamma * 400 * log10(e)
 
     @property
     def elo_uncertainty(self):
@@ -231,7 +235,7 @@ class Network(Model):
 
     @property
     def rating(self):
-        return f"{self.elo} ± {2 * self.elo_uncertainty:.1}"
+        return f"{self.elo:.{self.elo_precision}f} ± {2 * self.elo_uncertainty:.{self.elo_precision}f}"
 
     @property
     def model_download_url(self):
